f7e2555eb480d9a5f6dd7b39c8c036043eb1f8ad
Igor Ribeiro <ribeiro.igor642@gmail.com>
2025-01-22 20:46:55 -0300

    Domain model

---
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -0,0 +1,40 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+
+    private int position;
+
+    public Belonging() {
+    }
+
+    public Belonging(Game game, GameList list, int position) {
+        id.setGame(game);
+        id.setList(list);
+        this.position = position;
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public void setId(BelongingPK id) {
+        this.id = id;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+}
